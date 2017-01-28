9b0d5f33b2c4da398fb610f037322f7a5303f944
tanglie <tanglie23@163.com>
2017-01-29 00:54:35 +0800

    更新1.32

--- chapter 1/1.32.rkt
+++ chapter 1/1.32.rkt
@@ -10,12 +10,12 @@
         result
         (accumulateSingle (next a) (combiner result (term a)))))
   (accumulateSingle (+ a 1) (term a)))
-(accumulate plus cube 1 inc 3)
-(accumulate multiply cube 1 inc 3)
 
 (define (recursive-accumulate combiner term a next b)
-  (define (accumulateSingle a result)
-    cond((= a b) (term a))
-    ())
+  (cond((= a b) (term a))
+    (else (combiner (term b) (recursive-accumulate combiner term a next (- b 1))))
+    ))
 (accumulate plus cube 1 inc 3)
 (accumulate multiply cube 1 inc 3)
+(recursive-accumulate plus cube 1 inc 3)
+(recursive-accumulate multiply cube 1 inc 3)
