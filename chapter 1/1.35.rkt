6e2ff6fbed8de1ecbfc6f2dae8196d1a87f1d4a7
tanglie <tanglie23@163.com>
2017-01-29 11:16:24 +0800

    1.35

---
+++ chapter 1/1.35.rkt
@@ -0,0 +1,10 @@
+#lang racket
+(define tolerance 0.00001)
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (if (close-enough? guess (f guess))
+          (f guess) (try (f guess))))
+  (try first-guess))
+(fixed-point (lambda(x)(+ 1 (/ 1 x))) 1.0)
