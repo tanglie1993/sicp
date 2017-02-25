51ed22bb00d73d2cc7c27065ca4b4375cde4744f
tanglie1993 <tanglie23@163.com>
2017-02-25 16:26:44 +0800

    2.42

--- chapter 2/commons.rkt
+++ chapter 2/commons.rkt
@@ -1,23 +1,32 @@
+#lang racket
+(provide flatmap)
+(provide filter)
+(provide accumulate)
+(provide enumerate-interval)
+
+
 (define (flatmap proc seq)
-  (accumulate append nil (map proc seq)))
+  (accumulate append null (map proc seq)))
 (define (map proc items)
   (if (null? items)
-      nil
+      null
       (cons (proc (car items))
             (map proc (cdr items)))))
 (define (filter predicate sequence)
-  (cond ((null? sequence) nil)
+  (cond ((null? sequence) null)
         ((predicate (car sequence))
          (cons (car sequence)
                (filter predicate (cdr sequence))))
         (else (filter predicate (cdr sequence)))))
-define (accumulate op initial sequence)
-(if (null? sequence)
-initial
-(op (car sequence)
-(accumulate op initial (cdr sequence)))))
-(define (flatmap proc seq)
-(accumulate append nil (map proc seq)))
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
 (define (remove item sequence)
-(filter (lambda (x) (not (= x item)))
-sequence))
+  (filter (lambda (x) (not (= x item)))
+          sequence))
+(define (enumerate-interval low high)
+  (if (> low high)
+      null
+      (cons low (enumerate-interval (+ low 1) high))))
