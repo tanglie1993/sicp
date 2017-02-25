78746c3d47b39d4386f038a5269d99796257e9dc
tanglie1993 <tanglie23@163.com>
2017-02-25 21:23:41 +0800

    2.62

---
+++ chapter 2/2.62.rkt
@@ -0,0 +1,43 @@
+#lang racket
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((equal? x (car set)) true)
+        (else (element-of-set? x (cdr set)))))
+
+(define (adjoin-set x set)
+  (if (element-of-set? x set)
+      set
+      (cons x set)))
+
+(define (intersection-set set1 set2)
+  (if (or (null? set1) (null? set2))
+      '()
+      (let ((x1 (car set1)) (x2 (car set2)))
+        (cond ((= x1 x2)
+               (cons x1
+                     (intersection-set (cdr set1)
+                                       (cdr set2))))
+              ((< x1 x2)
+               (intersection-set (cdr set1) set2))
+              ((< x2 x1)
+               (intersection-set set1 (cdr set2)))))))
+
+(define (union-set set1 set2)
+  (cond ((and (null? set1) (null? set2)) '())
+        ((null? set1) set2)
+        ((null? set2) set1)
+      (else (let ((x1 (car set1)) (x2 (car set2)))
+        (cond ((= x1 x2)
+               (cons x1
+                     (union-set (cdr set1)
+                                       (cdr set2))))
+              ((< x1 x2)
+               (cons x1
+                     (union-set (cdr set1)
+                                       set2)))
+              ((< x2 x1)
+               (cons x2
+                     (union-set set1
+                                       (cdr set2)))))))))
+
+(union-set (list 1 2 5) (list 1 2 3 7))
